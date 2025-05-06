e71fad4595a673aef9529a3f3b8f5742952557b1
abbou <abboud.nabil@outlook.com>
2025-05-06 21:03:22 +0200

    finished

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <stack>  
+#include <iostream>
+#include <exception>
+
+template<typename T>
+
+class MutantStack : public std::stack<T>
+{
+public:
+    MutantStack();
+    MutantStack(const MutantStack& other);
+    MutantStack& operator=(const MutantStack& other);
+    ~MutantStack();
+
+    typedef typename std::stack<T>::container_type::iterator iterator;
+
+    iterator begin();
+    iterator end();
+};
+
+#include "MutantStack.tpp"
